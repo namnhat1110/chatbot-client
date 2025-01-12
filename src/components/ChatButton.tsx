55c751e9c42ec1b3757648d15a8442b90281389a
NhatNam <namnhat1110@gmail.com>
2025-01-13 04:02:42 +0700

    remove redundant

--- src/components/ChatButton.tsx
+++ src/components/ChatButton.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
-
 export function ChatButton({ onToggle }: { onToggle: () => void }) {
   return (
-    <Button
-      className=" fixed bottom-4 right-4 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 transition"
+    <button
+      className="fixed p-3 bottom-4 right-4 bg-violet-400 text-white rounded-2xl shadow-lg hover:scale-125 hover:bg-violet-600 transition"
+      type="button"
       onClick={onToggle}
     >
       Chat
-    </Button>
+    </button>
   );
 }
--- src/components/SendButton.tsx
+++ src/components/SendButton.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
-
 export function SendButton({ onSend }: { onSend: () => void }) {
   return (
-    <Button
+    <button
       onClick={onSend}
       className="bg-blue-500 h-full text-white p-2 rounded-r-lg shadow-lg hover:bg-blue-600 transition"
     >
       Send
-    </Button>
+    </button>
   );
 }
